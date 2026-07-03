7de4f73810ec0a031d3f08d20d6b4afd2bd0f321
Darshan Makwana <darshanmakwana1305@gmail.com>
2026-07-03 20:17:15 +0530

    Create users table with primary key and constraints

--- ecommerce_database.sql
+++ ecommerce_database.sql
@@ -5,3 +5,17 @@
 CREATE DATABASE IF NOT EXISTS ecommerce_db;
 
 USE ecommerce_db;
+-- ==========================================
+-- USERS TABLE
+-- ==========================================
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) UNIQUE,
+    city VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
